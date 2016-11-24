74fadff77f12787d62d9d69d6d2d95f152b69734
Davide Pimpinella <davidepimpinella@10-20-48-214.0200.fed.it.iosda.org>
2016-11-24 16:41:25 +0100

    Initial Commit

---
+++ progettoProva/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  progettoProva
+//
+//  Created by Davide Pimpinella on 24/11/16.
+//  Copyright © 2016 AmazingGroup4. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
